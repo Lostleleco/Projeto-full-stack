e56c61ee491507e202eb1b1972fe0d0049562c40
Alex <alex_soares_oliveira@hotmail.com>
2024-11-10 14:24:16 -0300

    Chore: Initial Cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>teste</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-slate-200 p-4">
+          prod1
+        </div>
+        <div className="bg-slate-200 p-4">
+          prod1
+        </div>
+        <div className="bg-slate-200 p-4">
+          prod1
+        </div>
+        <div className="bg-slate-200 p-4">
+          prod1
+        </div>
+      </div>
+
     </div>
   );
 }

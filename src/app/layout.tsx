432a4267109ce4b71e6ec98169b9eb2e258fde0c
Alex <alex_soares_oliveira@hotmail.com>
2024-11-11 12:27:52 -0300

    Styles: Images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import clsx from "clsx";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -23,9 +25,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={clsx("bg-slate-700", geistSans.variable, geistMono.variable, "antialiased")}>
         <Navbar />
-        <main className="bg-slate-700 h-screen p-16">
+        <main className="h-screen p-16">
           {children}
         </main>
       </body>

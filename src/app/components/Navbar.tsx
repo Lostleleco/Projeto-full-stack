c80b37c0c606e5b30d6f835f97a1ef9d978e5898
Lostleleco <alex_soares_oliveira@hotmail.com>
2024-11-15 09:15:10 -0300

    Styles: Icon Cart

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { UserButton, SignInButton } from "@clerk/nextjs";
 import { useCartStore } from "@/store";
 
 function Navbar() {
-    const userStore = useCartStore();
+    // const userStore = useCartStore();
 
     return (
         <>
@@ -15,16 +15,37 @@ function Navbar() {
                     Next Store
                 </Link>
                 <div className="flex items-center gap-8">
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                    <SignedOut>
-                        <SignInButton mode="modal">
-                            <button className="border rounded-md border-gray-400 px-3 py-2">
-                                Fazer Login
-                            </button>
-                        </SignInButton>
-                    </SignedOut>
+                    <div className="flex items-center cursor-pointer relative">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3.75 4.5h16.5l-1.5 9H5.25l-1.5-9zM7.5 19.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm12 0a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+                            />
+                        </svg>
+                        <span className="bg-teal-600 text-sm font-bold rounded-full h-5 w-5 flex items-center justify-center absolute left-3 bottom-3">
+                            2
+                        </span>
+                    </div>
+                    <div>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                        <SignedOut>
+                            <SignInButton>
+                                <button className="border rounded-md border-gray-400 px-3 py-2">
+                                    Fazer Login
+                                </button>
+                            </SignInButton>
+                        </SignedOut>
+                    </div>
                 </div>
             </nav>
         </>
